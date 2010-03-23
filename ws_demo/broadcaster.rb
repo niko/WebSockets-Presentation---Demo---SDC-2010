e02bc5d8695e4fbec91278554caa0e29d507a0fc
Niko Dittmann <niko@restorm.com>
2010-03-23 09:53:54 +0100

    . broadcaster cleanup

--- ws_demo/broadcaster.rb
+++ ws_demo/broadcaster.rb
@@ -6,19 +6,23 @@ require 'em-websocket'
 
 EventMachine.run do
 
-  @sockets = []
+  @clients = []
+  @msg_count = 0
 
-  EventMachine::WebSocket.start(:host => '0.0.0.0', :port => 8080) do |socket|
-    socket.onopen do
-      @sockets << socket
-      puts @sockets.inspect
+  EventMachine::WebSocket.start(:host => '0.0.0.0', :port => 8080) do |ws|
+    ws.onopen do
+      @clients << ws
+      puts @clients.inspect
     end
 
-    socket.onmessage do |data|
-      @sockets.each  do |s|
-        puts "#{@sockets.index socket},#{data}"
-        s.send "#{@sockets.index socket},#{data}"
-      end
+    ws.onmessage do |msg|
+      @clients.each { |s| s.send "#{@clients.index ws},#{msg}" }
+      print '.' * @clients.size
+    end
+
+    ws.onclose do
+      @clients.delete ws
+      puts @clients.inspect
     end
   end # EM::WS
 
