652d97984f04ded7bbdcb1aeec0954615fbe4652
Niko Dittmann <niko@restorm.com>
2010-04-09 20:06:20 +0200

    + complete Tweet overhaul

--- ws_demo/twitter_streamer.rb
+++ ws_demo/twitter_streamer.rb
@@ -1,27 +1,67 @@
-#!/usr/bin/env ruby
-
 require 'rubygems'
 require 'eventmachine'
 require 'em-websocket'
+require 'twitter/json_stream'
 
-require 'active_support'
-require 'yajl'
 
 lib_path = File.expand_path(File.join(File.dirname(__FILE__), '..', 'lib'))
 $LOAD_PATH.unshift lib_path unless $LOAD_PATH.include?(lib_path)
- 
-require 'twitter/json_stream'
 
+require 'ostruct'
+require 'action_view'
+require 'action_view/helpers'
+require 'active_support'
+require 'yajl'
+require 'htmlentities'
+
+module HTML
+  extend self
+  def ec(text)
+    @encoder ||= HTMLEntities.new
+    @encoder.encode text, :hexadecimal
+  end
+end
+
+SEARCH = ['music', 'eat', 'drink']
+
+class Tweet < OpenStruct
+  include ActionView::Helpers
+
+  def initialize(json)
+    attrs = Yajl::Parser.parse(json)
+    super attrs if attrs.is_a? Hash
+  end
+
+  def user_name
+    user["name"]
+  end
+
+  def text_as_html
+    auto_link(HTML.ec text).gsub(/(#{SEARCH.join('|')})/i, "<b>\\1</b>")
+  end
+
+  def to_s
+    "#{user_name} (#{created_at}): #{text}"
+  end
+
+  def img_as_html
+    %Q{<img src="#{ user['profile_image_url'] }">}
+  end
+
+  def to_html
+    %Q{#{img_as_html}#{HTML.ec user_name} (#{created_at}): #{text_as_html}}
+  end
+end
+
+@sockets = []
+EM.kqueue = true
 EventMachine.run do
-
   @twitter_stream = Twitter::JSONStream.connect(
     :path    => '/1/statuses/filter.json',
-    :auth    => File.open('ws_demo/.twitter_auth').read.chomp,
+    :auth    => File.open(File.join(File.dirname(__FILE__), '.twitter_auth')).read.chomp,
     :method  => 'POST',
-    :content => 'track=music'
+    :content => "track=#{SEARCH.join(',')}"
   )
-
-  @sockets = []
 
   EventMachine::WebSocket.start(:host => '0.0.0.0', :port => 8080) do |socket|
     socket.onopen do
@@ -30,18 +70,15 @@ EventMachine.run do
     socket.onclose do
       @sockets.delete @socket
     end
-
-    @twitter_stream.each_item do |item|
-      @sockets.each do |ws|
-        msg = Yajl::Parser.parse(item)
-        name = msg["user"]["name"]
-        time = Time.parse(msg["created_at"]).to_s(:long)
-        msg = msg["text"].gsub(/(music)/i, "<b>\\1</b>")
-        puts msg
-
-        ws.send %Q{#{name} (#{time}): #{msg}\n}
-      end
-    end
   end # EM::WS
+
+  @twitter_stream.each_item do |item|
+    tweet = Tweet.new(item)
+    puts tweet.to_s
+    STDOUT.flush
+    @sockets.each do |ws|
+      ws.send tweet.to_html
+    end
+  end
 
 end # EM.run
