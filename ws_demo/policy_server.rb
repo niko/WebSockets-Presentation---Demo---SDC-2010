09ead8c841a86cd5ae9ed296594e4ab8b6bae9b2
Niko Dittmann <niko@restorm.com>
2010-04-09 20:50:26 +0200

    - puts statements from policy_server

--- ws_demo/policy_server.rb
+++ ws_demo/policy_server.rb
@@ -1,14 +1,8 @@
 require 'rubygems'
 require 'eventmachine'
 
-
 module PolicyServer
-  def post_init
-    puts "-- someone connected to the echo server!"
-  end
-
   def receive_data data
-    puts "-- received #{data}, sending policy file"
     policy = <<EOF
 <?xml version="1.0"?>
 <!DOCTYPE cross-domain-policy SYSTEM "http://www.macromedia.com/xml/dtds/cross-domain-policy.dtd">
@@ -16,12 +10,7 @@ module PolicyServer
   <allow-access-from domain="*" to-ports="*" />
 </cross-domain-policy>
 EOF
-    puts policy
     send_data policy and close_connection_after_writing
-  end
-
-  def unbind
-    puts "-- someone disconnected from the echo server!"
   end
 end
 
